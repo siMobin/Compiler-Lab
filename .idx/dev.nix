6a491dc6d6c9204bda65e8d3ddc86148bbb1847e
shakibulislammobin <shakibulislammobin@gmail.com>
2025-02-24 15:58:52 +0000

    .

---
+++ .idx/dev.nix
@@ -0,0 +1,48 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.gcc 
+    pkgs.flex
+    pkgs.bison
+    pkgs.python311
+    pkgs.python311Packages.pip
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "luniclynx.lex"
+      "lexical-lsp.lexical"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # WebPreview
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
